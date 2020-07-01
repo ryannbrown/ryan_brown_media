a636dc7ed8e95ae0cc628f9d5f5c0baa4c4e9047
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-07-01 10:33:47 -0400

    style and contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,8 @@
 import { Container, Nav, Button, Card, Col, Row } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 // import logo from './logo.svg';
 import './style.css';
 
@@ -11,25 +13,24 @@ function Contact() {
 
         <div className="about-page">
 
-            
+
             <div className="wrapper">
+                <div><Row><Col><div className="headline">Need work done? </div></Col></Row></div>
+                <Row><Col><p>Just throw me an email or give me a call and we can make a plan.</p></Col></Row>
                 <Row>
                     <Col>
-                    <div>
-                        <h1>Do you need a website for your business?</h1>
-                    </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                    <div>
-                        <h1>Email</h1>
-                    </div>
+                        <a href="mailto:ryanbrownmedia@gmail.com?subject=Interested%20In%20Web%20Design">
+                            <div style={{ fontSize: '32px' }}>
+                                <FontAwesomeIcon icon={faEnvelope} />
+                            </div>
+                        </a>
                     </Col>
                     <Col>
-                    <div>
-                        <h1>Call</h1>
-                    </div>
+                    <a href="tel:19198165074">
+                        <div style={{ fontSize: '32px' }}>
+                            <FontAwesomeIcon icon={faPhone} />
+                        </div>
+                        </a>
                     </Col>
                 </Row>
 
