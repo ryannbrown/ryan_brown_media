80a3200e1e9ccc94205526cca367fd225619a25b
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-07-01 12:45:58 -0400

    fixed issues for now?

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -31,10 +31,10 @@ function Navigation() {
         <Navbar.Collapse id="basic-navbar-nav">
 
           <Nav className="mr-auto">
-            <Nav.Link href="portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="pricing">Pricing</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/pricing">Pricing</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       
